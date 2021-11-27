99cb3909e248d4267462c70ef03f46460efd9a2c
Senthur Kumaran <senthurkumaran@adventus.io>
2021-11-27 13:32:24 +0530

    some small adjustments

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -7,22 +7,22 @@ const resolvers = {
         tracksForHome: (_, __, { dataSources }) => {
             return dataSources.trackAPI.getTracksForHome();
         },
-        tracksForHomeFetch: async () => {
-            const baseUrl = "https://odyssey-lift-off-rest-api.herokuapp.com";
-            const res = await fetch(`${baseUrl}/tracks`);
-            return res.json();
-        },
+        // tracksForHomeFetch: async () => {
+        //     const baseUrl = "https://odyssey-lift-off-rest-api.herokuapp.com";
+        //     const res = await fetch(`${baseUrl}/tracks`);
+        //     return res.json();
+        // },
     },
     Track: {
-        // author: ({ authorId }, _, { dataSources }) => {
-        //     return dataSources.trackAPI.getAuthor(authorId);
-        // }
+        author: ({ authorId }, _, { dataSources }) => {
+            return dataSources.trackAPI.getAuthor(authorId);
+        }
         // using fetch instead of dataSources
-        author: async ({ authorId }, _, { dataSources }) => {
-            const baseUrl = "https://odyssey-lift-off-rest-api.herokuapp.com";
-            const res = await fetch(`${baseUrl}/author/${authorId}`);
-            return res.json();
-        },
+        // author: async ({ authorId }, _, { dataSources }) => {
+        //     const baseUrl = "https://odyssey-lift-off-rest-api.herokuapp.com";
+        //     const res = await fetch(`${baseUrl}/author/${authorId}`);
+        //     return res.json();
+        // },
     }
 };
 
